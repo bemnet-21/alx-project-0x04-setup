c24900f1aa027fd1733e9f277a1536ea1c3c3e8a
bemnet-21 <anjejobemnet@gmail.com>
2025-06-15 03:37:00 +0300

    imperative routing

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -9,9 +9,9 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
